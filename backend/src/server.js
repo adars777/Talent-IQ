3b4b451e4efa28b640f1de4941a8e4c08689aff4
adarsh <adarshnishadd777@gmail.com>
2026-03-18 22:31:26 +0530

    SETUP and First commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import path from "path";
+
+import { ENV } from "./lib/env.js";
+
+const app = express();
+const __dirname = path.resolve();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running..." });
+});
+
+
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is the book endpoint..." });
+});
+
+
+// make our app ready for development
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () =>
+  console.log("server is running on PORT: ", ENV.PORT),
+);
