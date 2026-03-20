e8728dea8a470fa1bad2a94dc8966d1dc3479d09
adarsh <adarshnishadd777@gmail.com>
2026-03-20 23:59:36 +0530

    Refactor code structure for improved readability and maintainability

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 how to store user data in DB using Clerk?
 CLERK ==>  INNGEST(background job=> function ) - MONGODB 
+
+
+WebHooks => webhooks are automated message that are sent when something happens
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import path from "path";
-
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { serve } from "inngest/express";
 
 const app = express();
 const __dirname = path.resolve();
+
+// /middlewares
+app.use(express.json());
+
+// Credentials:true meaning? =>server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, Credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running..." });
@@ -16,14 +24,13 @@ app.get("/books", (req, res) => {
 });
 
 // make our app ready for development
-if (ENV.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
 
 const startserver = async () => {
   try {
